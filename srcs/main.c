974008cb6b9ec3e1b60d06dfefa75f1022bde6fe
gmp <gmp@MacBook-Pro-de-gmp.local>
2014-04-23 18:03:21 +0200

    modif main

--- srcs/main.c
+++ srcs/main.c
@@ -11,7 +11,15 @@
 /* ************************************************************************** */
 
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <mach-o/loader.h>
+#include <sys/mman.h>
+#include <stab.h>
+#include <nlist.h>
 #include "header.h"
+
 
 void		ft_file_error(char *file)
 {
@@ -20,17 +28,93 @@ void		ft_file_error(char *file)
 	ft_putstr(" (No such file or directory)\n");
 }
 
+void        ft_nm(char *file)
+{
+    int fd;
+    struct stat stat_buf;
+    size_t size;
+
+    char *addr = NULL;
+    struct mach_header_64 *mh;
+    struct load_command *lc;
+    struct segment_command_64 *sc;
+    struct symtab_command      *test;
+    struct section_64 *sect;
+
+    // Open the file and get its size
+    fd = open(file, O_RDONLY);
+    fstat(fd, &stat_buf);
+    size = stat_buf.st_size;
+
+    // Map the file to memory
+    addr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_FILE | MAP_PRIVATE, fd, 0);
+
+    // The first bytes of a Mach-O file comprise its header
+    mh = (struct mach_header_64 *)addr;
+
+    // Load commands follow the header
+    addr += sizeof(struct mach_header_64);
+
+    printf("There are %d load commands\n", mh->ncmds);
+
+    for (int i = 0; (unsigned int)i < mh->ncmds; i++) {
+        lc = (struct load_command *)addr;
+        if (lc->cmdsize == 0) continue;
+
+        // If the load command is a (64-bit) segment,
+        // print information about the segment
+        sc = (struct segment_command_64 *)addr;
+        if (lc->cmd == LC_SEGMENT_64) {
+            sect = (struct section_64 *)addr;
+            test = (struct symtab_command *)addr;
+            printf("Segment %s\n\t"
+                "vmaddr 0x%llx\n\t"
+                "vmsize 0x%llx\n\t"
+                "fileoff %llu\n\t"
+                "filesize %llu\n\t"
+                "cmd %d\n\t"
+                "section : %s\n",
+                sc->segname,
+                sc->vmaddr,
+                sc->vmsize,
+                sc->fileoff,
+                sc->filesize,
+                sc->cmd,
+                sect->sectname);
+        }
+
+        // Advance to the next load command    
+        addr += lc->cmdsize;
+    }
+
+    printf("\nDone.\n");
+
+    munmap(addr, size);
+    close(fd);
+}
+
+/*
 void		ft_nm(char *file)
 {
-	if (open(".", O_RDONLY) != -1)
+    struct stat    test;
+
+
+	if (open(file, O_RDONLY) >= 0)
 	{
 		write(1, file, ft_strlen(file));
 		write(1, "\n", 1);
 	}
 	else
+    {
 		ft_file_error(file);
+        return ;
+    }
+    stat(file, &test);
+    printf("peripherique : %d\n", test.st_dev);
+    printf("proprietaire : %d\n", test.st_uid);
+    printf("stat : %lld\n", test.st_size);
 }
-
+*/
 int			main(int ac, char **av)
 {
 	int		i;
